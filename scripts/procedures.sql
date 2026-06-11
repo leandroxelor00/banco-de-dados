c2d35f6f7a01bab1dac0d33cbdb4fc449f6b1d78
ld.slvrei <leonardodias080603@gmail.com>
2026-06-11 11:57:04 -0300

    vai tomando dos meno que tem odio
    procedure

--- scripts/procedures.sql
+++ scripts/procedures.sql
@@ -1,1 +1,85 @@
-CREATE PROCEDURE
+DELIMITER //
+
+CREATE PROCEDURE BuscarFornecedor(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM fornecedores
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
+
+CALL BuscarFornecedor(1);
+
+
+DELIMITER //
+
+CREATE PROCEDURE BuscarEmpresa(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM empresa
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE BuscarPassageiro(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM passageiros
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE BuscarLocalidade(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM localidades
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
+
+
+
+DELIMITER //
+
+CREATE PROCEDURE BuscarTarifa(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM tarifa
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE PROCEDURE BuscarSolicitacao(
+    IN p_id INT
+)
+BEGIN
+    SELECT *
+    FROM solicitacao
+    WHERE id = p_id;
+END//
+
+DELIMITER ;
